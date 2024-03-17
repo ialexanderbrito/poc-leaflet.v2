ce4c94d3d677a43cfff4a06328b0294782183a41
ialexanderbrito <xanderzinho26@gmail.com>
2024-03-16 22:19:59 -0300

    feat: add app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { LeafletMap } from './pages/LeafletMap';
+
+import './App.css';
+import 'leaflet/dist/leaflet.css';
+import 'sweetalert2/src/sweetalert2.scss';
+
+function App() {
+  return <LeafletMap />;
+}
+
+export default App;
