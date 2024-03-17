98223b6b18767615b366ab433a00f61c9d69f5a1
ialexanderbrito <xanderzinho26@gmail.com>
2024-03-16 22:19:17 -0300

    feat: add component maps

---
+++ src/components/Maps.tsx
@@ -0,0 +1,47 @@
+import { Circle, Map, Marker, TileLayer } from 'react-leaflet';
+
+import { useLeaflet } from '../hooks/useLeaflet';
+
+interface MapsProps {
+  center: {
+    latitude: number;
+    longitude: number;
+  };
+  position: {
+    latitude: number;
+    longitude: number;
+  };
+  radius: number;
+  enableRadius: boolean;
+}
+
+export function Maps({ center, position, radius, enableRadius }: MapsProps) {
+  const { mapIcon, handleMapClick } = useLeaflet();
+
+  return (
+    <Map
+      center={[center.latitude, center.longitude]}
+      zoom={15}
+      style={{ height: '100vh', width: '100%' }}
+      onClick={handleMapClick}
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+      {position.latitude !== 0 && (
+        <Marker
+          position={[position.latitude, position.longitude]}
+          icon={mapIcon}
+          interactive={false}
+        />
+      )}
+
+      {enableRadius && (
+        <Circle
+          center={[position.latitude, position.longitude]}
+          radius={radius}
+          color="blueviolet"
+        />
+      )}
+    </Map>
+  );
+}
